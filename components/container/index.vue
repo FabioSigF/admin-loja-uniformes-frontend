b68fe28a60d59480c46ea636853510396a8f22c1
Fábio Signorini <95833161+FabioSigF@users.noreply.github.com>
2024-10-17 22:00:34 -0300

    initial setup

---
+++ components/container/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="bg-white rounded p-4">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
